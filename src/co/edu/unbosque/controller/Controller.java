c42da622a2f98dbe1fc75fd49be178c1644f3e08
estebanCastr0 <83050986+estebanCastr0@users.noreply.github.com>
2021-11-01 15:10:38 -0500

    Se creo la version inicial del trabajo

---
+++ src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,20 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.view.ViewConsole;
+
+public class Controller {
+
+    private ViewConsole v;
+
+    public Controller (){
+        	v = new ViewConsole();
+        	start();
+    }
+    
+    public void start() {
+
+    	
+    }
+    
+
+}
